1d25f8e7ddf697e816ea8c74f700776b208b0d54
S.Nasiri <S.Nasiri>
2026-05-31 14:37:07 +0330

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('ngs-1000-separator');
+}
